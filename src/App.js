97ca8f2ccdb217ed53410ef2fa9dfde8bb65f247
914sanju <914sanju@gmail.com>
2025-01-09 13:58:58 +0530

    this is demo file

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from './Layout/Navbar';
+import logo from './logo.svg';
+
+
+function App() {
+  return (
+    <div className="App">
+    <h1 className='head'>hello</h1>
+    <Navbar/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Layout/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from '../Assets/img1.jpg'
+
+function Navbar() {
+  return (
+    <div className='navbar' >
+        <div className='img'>
+            <img src={logo} alt='img'></img>
+             </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
